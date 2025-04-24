1275c3388da1df5a035e7b86ec740c8d3626a196
YzBELLA2 <erandas905@gmail.com>
2025-04-24 20:21:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~ZpoXRTaa#2eYzeyYQxWpZIoyBoVReJOkrxcuQyzo_AmwG5-RQyEk' 
 //Enter your 𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿 session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
@@ -29,7 +29,7 @@ global.ownernumber = process.env.OWNER_NUMBER || '233509977126'
 
 //-----------------[ OWNER NAME ]------------------//
 
-global.ownername = process.env.OWNER_NAME || 'Hector Manuel' 
+global.ownername = process.env.OWNER_NAME || 'eranda sampath' 
 
 //------------[ STICKER PACKNAME ]-----------------//
 
